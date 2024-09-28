d5e2d29bf8788b78626a18b5c76ed2e258830377
Roger <rogeriskrenov@gmail.com>
2024-09-29 01:57:14 +0200

    learning a bit more about working with databases in Django

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
