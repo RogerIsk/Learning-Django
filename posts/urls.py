c49adeccc49f2fb2bcad7258751c903998bf21c7
Roger <rogeriskrenov@gmail.com>
2024-10-01 16:08:15 +0200

    commit

--- posts/urls.py
+++ posts/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.posts_list),
+    path('', views.posts_list, name='list'),
+    path('<slug:slug>', views.post_page, name='page'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from.models import Post
 
 def posts_list(request):
-    return render(request, 'posts/posts_list.html')
+    posts = Post.objects.all().order_by('-date')
+    return render(request, 'posts/posts_list.html', {'posts': posts})
+
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/post_page.html', {'post': post})
